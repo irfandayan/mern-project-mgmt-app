c42e78ee36aed9aaff7ae36b839400b733208be9
irfan <engr.irfandayan@gmail.com>
2022-08-15 15:53:53 +0500

    Setup Apollo Client & get clients

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const colors = require("colors");
 require("dotenv").config();
@@ -12,6 +13,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
