686e5f0551c7f843a590dcb0ef60aa87f3161813
irfan <engr.irfandayan@gmail.com>
2022-08-16 11:39:33 +0500

    Add input fields for add client

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 export const AddClientModal = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.concat([addClient]) },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, phone);
+  };
+
   return (
     <>
       <button
@@ -38,11 +60,45 @@ export const AddClientModal = () => {
               ></button>
             </div>
             <div className="modal-body">
-              <form>
+              <form onSubmit={onSubmit}>
                 <div className="mb-3">
                   <label className="form-label">Name</label>
-                  <input type="text" className="form-control" id="name" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
+                <div className="mb-3">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label className="form-label">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+                <button
+                  type="Submit"
+                  data-bs-dismiss="modal"
+                  className="btn btn-secondary"
+                >
+                  Submit
+                </button>
               </form>
             </div>
           </div>
